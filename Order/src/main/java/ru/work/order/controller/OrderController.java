fa4392bfab3c5230d8a7f2a895acf1557b624df2
nvk <bespalov0928@mail.ru>
2024-09-23 20:27:55 +0300

    second

--- Order/src/main/java/ru/work/order/controller/OrderController.java
+++ Order/src/main/java/ru/work/order/controller/OrderController.java
@@ -3,12 +3,12 @@ package ru.work.order.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.work.order.model.Order;
 import ru.work.order.service.OrderService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -19,5 +19,10 @@ public class OrderController {
     public ResponseEntity<Order> saveOrder(@RequestBody Order order) {
         orderService.save(order);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List> findAll(){
+        return new ResponseEntity<List>(HttpStatus.OK);
     }
 }
